bd451fb3fe6291e420faf95799958204f9ca56b7
José Eduardo Rocha Peres <joseeduardorperes@gmail.com>
2024-03-31 17:58:55 -0300

    test: refatorando e adicionando testes do userService

--- src/tests/unit/services/userService.test.ts
+++ src/tests/unit/services/userService.test.ts
@@ -1,52 +1,170 @@
-import { afterAll, describe, expect, it } from '@jest/globals';
+import { afterEach, describe, expect, jest, test } from '@jest/globals';
 
 import UserService from '../../../services/userService';
-import { IBody as IUserData } from '../../../types/createUser';
 
 import db from '../../../database';
 
-describe.skip('UserService', () => {
-	afterAll((done) => {
-		db.truncate('Users').then(() => {
-			db.disconnect().then(() => done());
+import { IBody as IUserData } from '../../../types/createUser';
+import { TAccountType } from '../../../types/user';
+import { IUserInsertData, IUserResult } from '../../../types/database';
+
+function createUserProfile(profileType: TAccountType): IUserData {
+	return {
+		name: 'Default User',
+		email: 'default.user@test.com',
+		password: '12345',
+		accountType: profileType,
+	};
+}
+
+function createUser(userData: IUserData): IUserResult {
+	return {
+		id: '1',
+		name: userData.name,
+		email: userData.email,
+		password: userData.password,
+		createdAt: '2024-31-03',
+		updatedAt: '2024-31-03',
+	};
+}
+
+function mockFindByEmailResult(user?: IUserResult) {
+	db.findUserByEmail = jest
+		.fn<() => Promise<IUserResult | undefined>>()
+		.mockImplementation(() => {
+			if (user) {
+				return Promise.resolve(user);
+			}
+
+			return Promise.resolve(undefined);
 		});
+}
+
+function mockFindProfileId(profileType?: TAccountType) {
+	db.findUserProfileId = jest
+		.fn<() => Promise<any>>()
+		.mockImplementation(() => {
+			if (profileType) {
+				return Promise.resolve({
+					[profileType + 'Id']: '1',
+					userId: '1',
+				});
+			}
+
+			return Promise.resolve(undefined);
+		});
+}
+
+function mockAddToProfile() {
+	db.addUserToProfile = jest.fn<() => Promise<void>>().mockResolvedValue();
+}
+
+function mockAddUserResult({ name, email, password }: IUserInsertData) {
+	db.addUser = jest.fn<() => Promise<IUserResult>>().mockResolvedValue({
+		id: '1',
+		name,
+		email,
+		password,
+		createdAt: '2024-31-03',
+		updatedAt: '2024-31-03',
+	});
+}
+
+describe('UserService', () => {
+	let defaultUser: IUserData;
+	let defaultUserResult: IUserResult;
+
+	afterEach(() => {
+		defaultUser = {} as IUserData;
+		defaultUserResult = {} as IUserResult;
+		jest.clearAllMocks();
 	});
 
-	it('Should add a new patient user', async () => {
-		const userData: IUserData = {
-			name: 'patient user',
-			email: 'patient@example.com',
-			password: '12345',
-			accountType: 'patient',
-		};
+	describe('createUser', () => {
+		test.each(['patient', 'caregiver'] as TAccountType[])(
+			'Should throw an error 409 when user already registered with profile type %s',
+			async (profileType) => {
+				// arrange
+				defaultUser = createUserProfile(profileType);
+				defaultUserResult = createUser(defaultUser);
 
-		const newUser = await UserService.createUser(userData);
+				mockFindByEmailResult(defaultUserResult);
+				mockFindProfileId(profileType);
 
-		expect(newUser).toHaveProperty('id');
-		expect(newUser.id).toBeTruthy();
-		expect(newUser).toHaveProperty('name', userData.name);
-		expect(newUser).toHaveProperty('email', userData.email);
-		expect(newUser).toHaveProperty('accountType', userData.accountType);
+				// act
+				await UserService.createUser(defaultUser)
+					// assert
+					.then((value) => {
+						expect(value).toBeUndefined();
+					})
+					.catch((err) => {
+						expect(err).toHaveProperty(
+							'message',
+							'You already have an account like ' + profileType
+						);
+						expect(err).toHaveProperty('statusCode', 409);
+					});
+			}
+		);
+
+		test.each(['patient', 'caregiver'] as TAccountType[])(
+			'Should add a user to profile type %s',
+			async (profileType) => {
+				defaultUser = createUserProfile(profileType);
+				defaultUserResult = createUser(defaultUser);
+
+				mockFindByEmailResult(defaultUserResult);
+				mockFindProfileId();
+				mockAddToProfile();
+
+				// act
+				const user = await UserService.createUser(defaultUser);
+
+				// assert
+				expect(user).toStrictEqual(
+					expect.objectContaining({
+						id: defaultUserResult.id,
+						name: defaultUserResult.name,
+						email: defaultUserResult.email,
+						accountType: profileType,
+					})
+				);
+			}
+		);
+
+		test.each(['patient', 'caregiver'] as TAccountType[])(
+			'Should create a new %s type user',
+			async (profileType) => {
+				// arrange
+				defaultUser = createUserProfile(profileType);
+				defaultUserResult = createUser(defaultUser);
+
+				mockFindByEmailResult();
+				mockAddToProfile();
+				mockAddUserResult({
+					name: defaultUserResult.name,
+					email: defaultUserResult.email,
+					password: defaultUser.password,
+				});
+
+				// act
+				const user = await UserService.createUser({
+					name: defaultUserResult.name,
+					email: defaultUserResult.email,
+					password: defaultUser.password,
+					accountType: profileType,
+				});
+
+				// assert
+				expect(user).toStrictEqual(
+					expect.objectContaining({
+						id: defaultUserResult.id,
+						name: defaultUserResult.name,
+						email: defaultUserResult.email,
+						accountType: profileType,
+					})
+				);
+			}
+		);
 	});
-
-	it('Should add a new caregiver user', async () => {
-		const userData: IUserData = {
-			name: 'caregiver user',
-			email: 'caregiver@example.com',
-			password: '12345',
-			accountType: 'caregiver',
-		};
-		const newUser = await UserService.createUser(userData);
-		expect(newUser).toHaveProperty('id');
-		expect(newUser.id).toBeTruthy();
-		expect(newUser).toHaveProperty('name', userData.name);
-		expect(newUser).toHaveProperty('email', userData.email);
-		expect(newUser).toHaveProperty('accountType', userData.accountType);
-	});
-
-	it('Should check if email is already registered', async () => {});
-
-	it('Should check which user profile type(patient/caregiver) is registered', async () => {});
-
-	it('Should throw an error if user tries to register for the same profile type', async () => {});
 });
