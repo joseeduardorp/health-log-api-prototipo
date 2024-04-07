61db720e47f9c8d1e95dd7dc784c108d896f95ff
José Eduardo Rocha Peres <joseeduardorperes@gmail.com>
2024-04-07 20:47:10 -0300

    test: refatorando e adicionando testes unitários para a classe Database

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,23 +3,40 @@ import knex, { Knex } from 'knex';
 import config from '../../config';
 
 class Database {
-	public readonly client: Knex;
+	private client: Knex | null;
 
 	constructor() {
-		this.client = knex({
-			client: 'pg',
-			connection: {
-				host: config.postgresHost,
-				port: config.postgresPort,
-				user: config.postgresUser,
-				password: config.postgresPassword,
-				database: config.postgresDb,
-			},
-		});
+		this.client = null;
+	}
+
+	async connect() {
+		if (!this.client) {
+			this.client = knex({
+				client: 'pg',
+				connection: {
+					host: config.postgresHost,
+					port: config.postgresPort,
+					user: config.postgresUser,
+					password: config.postgresPassword,
+					database: config.postgresDb,
+				},
+			});
+		}
 	}
 
 	async disconnect() {
-		await this.client.destroy();
+		if (this.client) {
+			await this.client.destroy();
+			this.client = null;
+		}
+	}
+
+	getClient() {
+		if (!this.client) {
+			throw new Error('Database not connected!');
+		}
+
+		return this.client;
 	}
 }
 
--- src/database/tests/index.test.ts
+++ src/database/tests/index.test.ts
@@ -1,11 +1,55 @@
-import { describe, expect, it } from '@jest/globals';
+import { afterEach, beforeEach, describe, expect, it } from '@jest/globals';
 
 import Database from '../';
 
 describe('Unit - Database', () => {
-	it('Should check instance', async () => {
-		const db = new Database();
+	let db: Database;
 
-		expect(db).toBeInstanceOf(Database);
+	beforeEach(async () => {
+		db = new Database();
+	});
+
+	afterEach(async () => {
+		await db.disconnect();
+	});
+
+	it('Should connect to the database if client is null', async () => {
+		expect(db['client']).toBeNull();
+
+		await db.connect();
+
+		expect(db['client']).toBeTruthy();
+	});
+
+	it('Should connect to the database if client is not null', async () => {
+		await db.connect();
+
+		expect(db['client']).toBeTruthy();
+
+		await db.connect();
+	});
+
+	it('Should disconnect to database', async () => {
+		await db.connect();
+
+		await db.disconnect();
+
+		expect(db['client']).toBeNull();
+	});
+
+	it('Should throw an error when getClient() is called before connecting', async () => {
+		const catchError = () => {
+			db.getClient();
+		};
+
+		expect(catchError).toThrowError('Database not connected!');
+	});
+
+	it('Should return Knex client when getClient() is called after connecting', async () => {
+		await db.connect();
+
+		const client = db.getClient();
+
+		expect(client).toBeTruthy();
 	});
 });
